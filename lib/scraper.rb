5c9969b9736c6c5227fdf43e0066bbcfcfa95dd1
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-11 01:14:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,20 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+  
  def get_courses
    self.get_page.css(".post")
  end
+ 
+ def make_courses
+   self.get_courses.each dp |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+end
+
+Scraper.new.print_courses 
  
 end
